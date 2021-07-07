bae77d20c5f4a39344bf8a009f6bf310db8e42db
danrayu <dan.rayu@gmail.com>
2021-07-07 12:49:37 +0300

    Saving before attempting RecyclerViewPool

--- app/src/main/java/com/example/exercisemanager/ui/groups/GroupsRVAdapter.kt
+++ app/src/main/java/com/example/exercisemanager/ui/groups/GroupsRVAdapter.kt
@@ -5,17 +5,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.exercisemanager.databinding.GroupItemBinding
+import kotlinx.android.synthetic.main.group_item.view.*
 
 class GroupsRVAdapter(
     private val groupList: MutableList<Group>,
     private val callback: EditEventInterface
 ) : RecyclerView.Adapter<GroupsRVAdapter.ViewHolder>() {
 
+    private lateinit var rvAdapter: ExerciseERVUneditableAdapter
+
     interface EditEventInterface {
         fun editGroupButtonPressed(group: Group, isNew:Boolean)
     }
 
-    inner class ViewHolder(val binding: GroupItemBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class ViewHolder(val binding: GroupItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val recyclerView : RecyclerView = itemView.rv_group_display
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = GroupItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -25,12 +30,21 @@ class GroupsRVAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         with(holder) {
             with(groupList[position]) {
+
+                rvAdapter = ExerciseERVUneditableAdapter(groupList[position].exercises)
+                holder.recyclerView.adapter = rvAdapter
+
                 binding.tvGroupName.text = this.name
                 binding.tvGdescription.text = this.description
                 binding.rvGroupDisplay.adapter = ExerciseERVUneditableAdapter(this.exercises)
                 binding.rlExpandedGroup.visibility = if (this.expanded) View.VISIBLE else View.GONE
+
                 binding.btnEditGroup.setOnClickListener {
                     callback.editGroupButtonPressed(this, false)
+                }
+                binding.groupCardLayoutG.setOnClickListener {
+                    this.expanded = !this.expanded
+                    notifyDataSetChanged()
                 }
             }
         }
